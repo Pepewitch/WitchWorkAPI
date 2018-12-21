5a52592ac5e64654f068685765fabae137d2298d
pepe_witch <skpepe134@gmail.com>
2018-12-21 09:00:03 +0700

    handle open and close with get method

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import api from './router';
+import morgan = require('morgan');
 
 const app = express();
 app.use(cors());
-app.use('/api' , api);
+app.use(morgan('combined'));
+app.use('/api', api);
 
 export default app;
--- src/router/embedded.ts
+++ src/router/embedded.ts
@@ -50,6 +50,100 @@ embedded.get('/', async (req, res) => {
   }
 });
 
+
+embedded.get('/open/:doorID', async (req, res) => {
+  const doorID = req.params['doorID'].trim() as string;
+  const exist = await checkExist(doorID);
+  if (!exist) {
+    return res.sendStatus(404);
+  }
+  if (doorID.length === 0) {
+    return res.status(400).send({ error: 'Invalid doorID' });
+  }
+  try {
+    const snapshot = await fs.doc(doorID).get();
+    let condition;
+    if (snapshot.exists) {
+      condition = snapshot.data();
+    } else {
+      condition = { from: null, to: null, whitelist: false };
+      await fs.doc(doorID).set(condition);
+    }
+    const update_value = checkWhitelist(condition)
+      ? {
+          status: 'open',
+          action: 'ring',
+        }
+      : {
+          status: 'open',
+          action: 'wait',
+        };
+
+    const update_status = realtime_database
+      .ref(`embedded/${doorID}`)
+      .update(update_value);
+    const update_transaction = realtime_database
+      .ref(`embedded_transactions/${doorID}`)
+      .push({
+        doorID,
+        ...update_value,
+        createdAt: new Date().toISOString(),
+        from: 'Embedded system',
+      });
+    await Promise.all([update_status, update_transaction]);
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(500);
+  }
+});
+
+embedded.get('/close/:doorID', async (req, res) => {
+  const doorID = req.params['doorID'];
+  const exist = await checkExist(doorID);
+  if (!exist) {
+    return res.sendStatus(404);
+  }
+  if (doorID.length === 0) {
+    return res.status(400).send({ error: 'Invalid doorID' });
+  }
+  try {
+    const snapshot = await fs.doc(doorID).get();
+    let condition;
+    if (snapshot.exists) {
+      condition = snapshot.data();
+    } else {
+      condition = { from: null, to: null, whitelist: false };
+      await fs.doc(doorID).set(condition);
+    }
+    const update_value = checkWhitelist(condition)
+      ? {
+          status: 'close',
+          action: 'wait',
+        }
+      : {
+          status: 'close',
+          action: 'wait',
+        };
+    const update_status = realtime_database
+      .ref(`embedded/${doorID}`)
+      .update(update_value);
+    const update_transaction = realtime_database
+      .ref(`embedded_transactions/${doorID}`)
+      .push({
+        doorID,
+        ...update_value,
+        createdAt: new Date().toISOString(),
+        from: 'Embedded system',
+      });
+    await Promise.all([update_status, update_transaction]);
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(500);
+  }
+});
+
 embedded.post('/open/:doorID', async (req, res) => {
   const doorID = req.params['doorID'].trim() as string;
   const exist = await checkExist(doorID);
